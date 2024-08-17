6882e5ca6fced13827ff8ed021d2d762389a9ebd
VC <VC@DESKTOP-DFGL07S>
2024-08-17 17:11:27 +0530

    17/08

---
+++ assignment4/src/arrays/ArrayfromPermutation.java
@@ -0,0 +1,21 @@
+package arrays;
+//1920. Build Array from Permutation
+import java.util.*;
+public class ArrayfromPermutation {
+
+	public static void main(String[] args) {
+
+        int list1[]={1,2,4};
+	    int list2[]={1,3,4};
+	    int list3[]=new int[list1.length+list2.length];
+	    int a=0;
+//	    for(int i=0;i<list3.length/2;i++){
+//	        list3[a++]=list1[i];
+//	        list3[a++]=list2[i];
+//	    }
+	    
+	    System.out.println(Arrays.toString(list3));
+		
+	}
+
+}
--- assignment5/src/collections/iterateelements.java
+++ assignment5/src/collections/iterateelements.java
@@ -5,16 +5,42 @@ public class iterateelements {
 	public static void main(String[] args) {
 
 		Scanner scan=new Scanner(System.in);
-		LinkedList ll=new LinkedList();
+		LinkedList<Integer> ll=new LinkedList<>();
 		ll.add(12);
 		ll.add(14);
 		ll.add(26);
 		
-		Iterator i=ll.iterator();
+		Iterator<Integer> i=ll.iterator();
 		while(i.hasNext()) {
-			System.out.println(i.next());
+			System.out.print(i.next()+" ");
 		}
+		System.out.println();
+		ListIterator j=ll.listIterator(1);
+		while(j.hasNext()) {
+			System.out.print(j.next()+" ");
+		}
+		System.out.println();
 		
+		Collections.sort(ll);
+		System.out.println(ll);
+		
+		Collections.swap(ll, 0, 2);
+		System.out.println(ll);
+		
+		HashSet<Integer> hs=new LinkedHashSet<>();
+		hs.add(12);
+		hs.add(14);
+		hs.add(16);
+		hs.add(26);
+		hs.add(16);
+		
+		System.out.println(hs);
+		HashSet<Integer> hs1=new LinkedHashSet<>();
+//		Iterator ii=hs.iterator();
+		for(int ii:hs) {
+			hs1.add(ii);
+		}
+		System.out.println(hs1);
 	}
 
 }
